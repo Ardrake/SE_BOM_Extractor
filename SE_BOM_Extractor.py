cbde4228f0cbf20671316173c53ee40137980d29
André Cooke <andrecooke@hotmail.com>
2014-04-04 10:30:04 -0400

    Added two collums to sql query to handle - report grouping - group by mais assembly commentary
    Brand new function tu get 2nd level assembly name - findNameByLevel

--- SE_BOM_Extractor.py
+++ SE_BOM_Extractor.py
@@ -15,13 +15,20 @@ objDocument = objApplication.ActiveDocument
 objOccurenceSets = objDocument.Occurrences
             
 def ExtractSubOccurrence(objOccurenceSets, level=1, nomenclature = []):
-    #Extract part object, in index 0, 2nd level of assembly� Index 1, part name, Index 2,3 and 4 are Length, Witdh and heigth
-    part = ["assembly", "part", "longueur", "largeur", "Epaisseur","Materiel"]
+    #Extract part object, in index 0, 2nd level of assembly Index 1, part name, Index 2,3 and 4 are Length, Witdh and heigth
+    part = ["groupname", "order", "assembly", "part", "longueur", "largeur", "Epaisseur","Materiel"]
+    
     for i in range(objOccurenceSets.Count):
         objSubOccurrences = objOccurenceSets.Item(i + 1)
         if objSubOccurrences.Subassembly == False:
-            VariableSet = objSubOccurrences.SubOccurrenceDocument.Variables
-            ModelsSet = objSubOccurrences.SubOccurrenceDocument.Models
+            try:
+                VariableSet = objSubOccurrences.SubOccurrenceDocument.Variables
+            except:
+                VariableSet = None
+            try:    
+                ModelsSet = objSubOccurrences.SubOccurrenceDocument.Models
+            except:
+                ModelsSet = None
             if CheckBody(ModelsSet):
                 SE_long = 0
                 SE_larg = 0 
@@ -30,48 +37,39 @@ def ExtractSubOccurrence(objOccurenceSets, level=1, nomenclature = []):
                     Variables = VariableSet.Item(i + 1)
                     if Variables.ExposeName == "SE_LONGUEUR":
                         SE_long = convert(Variables.Value)
-                        #print "     - %s : %f " % (Variables.ExposeName, convert(Variables.Value))
                     if Variables.ExposeName == "SE_LARGEUR":
                         SE_larg = convert(Variables.Value)
                     if Variables.ExposeName == "SE_EPAISSEUR":
                         SE_epai = convert(Variables.Value)
                 SE_Material = objSubOccurrences.SubOccurrenceDocument.Properties.Item(7).Item(1).Value
-                #This part needs to be optimised / refactored-------------**
-                objName = (level-1)
-                if level == 2:                    
-                    part = [objSubOccurrences.Parent.Name, objOccurenceSets.Parent.Name,  objSubOccurrences.Name, (SE_epai, SE_larg, SE_long),SE_Material]
-                if level == 3:                    
-                    part = [objSubOccurrences.Parent.Parent.Name, objOccurenceSets.Parent.Name,  objSubOccurrences.Name, (SE_epai, SE_larg, SE_long),SE_Material]
-                if level == 4:
-                    part = [objSubOccurrences.Parent.Parent.Parent.Name, objOccurenceSets.Parent.Name,  objSubOccurrences.Name, (SE_epai, SE_larg, SE_long),SE_Material]
-                if level == 5:
-                    part = [objSubOccurrences.Parent.Parent.Parent.Parent.Name, objOccurenceSets.Parent.Name,  objSubOccurrences.Name, (SE_epai, SE_larg, SE_long),SE_Material]
-                #----------------------------------------------------------**
+                SE_DocName = objSubOccurrences.SubOccurrenceDocument.Properties.Item(5).Item(1).Value
+                SE_TopName = findNameByLevel(objSubOccurrences,level-1)
+                if SE_DocName == '':
+                    SE_DocName = objSubOccurrences.Name
+                part = [assignGroupName(SE_TopName)[0],assignGroupName(SE_TopName)[1], SE_TopName, objOccurenceSets.Parent.Name, SE_DocName, (SE_epai, SE_larg, SE_long),SE_Material]
                 nomenclature.append(part)
-                #print nomenclature
         if objSubOccurrences.SubOccurrences != None: 
             ExtractSubOccurrence(objSubOccurrences.SubOccurrences, level+1)
+    
     return nomenclature
 
             
 def convert(val,frac=0.125):
     #convert decimal to inches
-    return  round((int((round(val,4) / 0.0254) / frac))*frac,4)  
+    return  round((int((round(val,8) / 0.0254) / frac))*frac,4)  
 
 def BuildQuery(BOM):
     # Build SQL Query to insert list data in SQL Table
-    keys_to_sql = "SE_top, SE_pere, SE_piece,SE_epaisseur,SE_largeur,SE_longueur,SE_Materiel"
+    keys_to_sql = "SE_groupname, SE_order, SE_top, SE_pere, SE_piece,SE_epaisseur,SE_largeur,SE_longueur,SE_Materiel"
     SQLQuery = "INSERT INTO SE_BOM("+keys_to_sql+")"
     tot_rec = 0
     for item in BOM:
         vars_to_sql = []
         tot_rec+=1
-        #print tot_rec
         for i in item:
             value_type = type(i)
             if value_type == tuple:
                 for n in i:
-                    #print (n , type(n))
                     vars_to_sql.append("'"+str(n)+"'")
             if value_type <> tuple:
                 vars_to_sql.append("'"+i+"'")
@@ -90,11 +88,34 @@ def CheckBody(ModelSet):
                 BodyValid = True     
     except:
         BodyValid = False
-    #print BodyValid
     return BodyValid
 
+def findNameByLevel(root,level):
+    currentElement = root;
+    for currentLevel in range(level):
+        currentElement = currentElement.Parent
+    # current element should now be the one we are searching for
+    SE_DocName = currentElement.OccurrenceDocument.Properties.Item("ProjectInformation").Item("Document Number").Value
+    if SE_DocName == "":
+        return currentElement.name
+    else :
+        return SE_DocName
 
+def assignGroupName(mur):
+    SE_Commentaire = objDocument.Properties.Item("SummaryInformation").Item("Commentaires").Value
+    listgroup = SE_Commentaire.splitlines()
+    result = ""
+    for idx, val in enumerate(listgroup):
+        if mur in val:
+            result =  ("MUR "+ val, str(idx+1))
+    if result == '':
+        result = ("MUR "+ mur, "9")
+    #print result
+    return result   
+   
 MP = ExtractSubOccurrence(objOccurenceSets)  
+
+#BuildQuery(MP)
 pySqlOperation.ZapSE_BOM()
 pySqlOperation.executeSQLQuery(BuildQuery(MP))    
     
